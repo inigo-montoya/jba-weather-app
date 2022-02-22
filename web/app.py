86969714f68f6fbe2047e6dd09e2a3ec214a8614
Joshua Lederman <inigo-montoya@users.noreply.github.com>
2022-02-22 13:27:10 -0800

    Update app.py for security scan

--- web/app.py
+++ web/app.py
@@ -102,6 +102,6 @@ def shutdown_session(exception=None):
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         arg_host, arg_port = sys.argv[1].split(':')
-        app.run(host=arg_host, port=arg_port, debug=True)
+        app.run(host=arg_host, port=arg_port, debug=False)
     else:
         app.run(debug=True)
